da3532f8adc3b6cf108f9c6b6744fb57a12336d8
HappyTiptoe <HappyTiptoe@users.noreply.github.com>
2022-11-02 15:05:45 +0000

    deploy: 71f6b1e487361467a2336e637f51ffc5faa001f1

---
+++ assets/froggo_tailwind.md.76284f3e.js
@@ -0,0 +1,22 @@
+import{_ as s,c as n,o as a,a as e}from"./app.074f018d.js";const u=JSON.parse('{"title":"Tailwind Template","description":"","frontmatter":{},"headers":[{"level":2,"title":"Helpful Links","slug":"helpful-links","link":"#helpful-links","children":[]},{"level":2,"title":"Project Structure","slug":"project-structure","link":"#project-structure","children":[]}],"relativePath":"froggo/tailwind.md","lastUpdated":1667401506000}'),l={name:"froggo/tailwind.md"},t=e(`<h1 id="tailwind-template" tabindex="-1">Tailwind Template <a class="header-anchor" href="#tailwind-template" aria-hidden="true">#</a></h1><h2 id="helpful-links" tabindex="-1">Helpful Links <a class="header-anchor" href="#helpful-links" aria-hidden="true">#</a></h2><ul><li><a href="https://vuejs.org/" target="_blank" rel="noreferrer">Vue</a></li><li><a href="https://vitejs.dev" target="_blank" rel="noreferrer">Vite</a></li><li><a href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">TypeScript</a></li><li><a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">Tailwind</a></li></ul><h2 id="project-structure" tabindex="-1">Project Structure <a class="header-anchor" href="#project-structure" aria-hidden="true">#</a></h2><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki"><code><span class="line"><span style="color:#A6ACCD;">.</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 src/</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2502\xA0\xA0 \u251C\u2500\u2500 assets/</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2502\xA0\xA0 \u251C\u2500\u2500 components/</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2502\xA0\xA0 \u251C\u2500\u2500 composables/</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2502\xA0\xA0 \u251C\u2500\u2500 css/</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2502\xA0\xA0 \u251C\u2500\u2500 public/</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2502\xA0\xA0 \u251C\u2500\u2500 App.vue</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2502\xA0\xA0 \u251C\u2500\u2500 env.d.ts</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2502\xA0\xA0 \u251C\u2500\u2500 index.html</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2502\xA0\xA0 \u2514\u2500\u2500 main.ts</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 .eslintrc.cjs</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 .gitignore</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 .prettierrc</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 package.json</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 postcss.config.cjs</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 README.md</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 tailwind.config.cjs</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 tsconfig.json</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u251C\u2500\u2500 tsconfig.node.json</span></span>
+<span class="line"><span style="color:#A6ACCD;">\u2514\u2500\u2500 vite.config.ts</span></span>
+<span class="line"><span style="color:#A6ACCD;"></span></span></code></pre></div>`,5),p=[t];function r(c,i,o,d,A,C){return a(),n("div",null,p)}const f=s(l,[["render",r]]);export{u as __pageData,f as default};
