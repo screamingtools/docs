0680a8c210ccb9260b8b13ea55b0338d7a72c4d7
eb <9453858-HappyTiptoe@users.noreply.gitlab.com>
2022-11-02 14:29:28 +0000

    feat: add data-attributes helpers section

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -17,206 +17,232 @@ export default defineConfig({
 
   themeConfig: {
     logo: '/favicon.ico',
-
     socialLinks: [{ icon: 'github', link: 'https://github.com/sf-designdev-packages' }],
-
-    nav: [
-      { text: 'Froggo', link: '/froggo/', activeMatch: '/froggo/*' },
-      { text: 'Share', link: '/share/', activeMatch: '/share/*' },
-      { text: 'Tables', link: '/tables/', activeMatch: '/tables/*' },
-      { text: 'Use', link: '/use/', activeMatch: '/use/*' },
-      { text: 'Utils', link: '/utils/', activeMatch: '/utils/*' }
-    ],
-
-    sidebar: {
-      '/froggo/': [
-        {
-          text: '@screaming/froggo',
-          items: [{ text: 'Getting Started', link: '/froggo/' }]
-        },
-        {
-          text: 'Templates',
-          collapsible: true,
-          items: [
-            { text: 'Tailwind', link: '/froggo/tailwind' },
-            { text: 'SASS', link: '/froggo/sass' },
-            { text: 'NPM Package', link: '/froggo/package' }
-          ]
-        }
-      ],
-      '/share/': [
-        {
-          text: '@screaming/share',
-          items: [{ text: 'Getting Started', link: '/share/' }]
-        },
-        {
-          text: 'Component',
-          collapsible: true,
-          items: [{ text: '<ShareLink />', link: '/share/share-link.md' }]
-        }
-      ],
-      '/tables/': [
-        {
-          text: '@screaming/tables',
-          items: [{ text: 'Getting Started', link: '/tables/' }]
-        },
-        {
-          text: 'Components',
-          collapsible: true,
-          items: [
-            { text: '<Table />', link: '/tables/table' },
-            { text: '<MiniTable />', link: '/tables/mini-table' }
-          ]
-        },
-        {
-          text: 'Helpers',
-          collapsible: true,
-          items: [
-            { text: 'createClasses', link: '/tables/helpers/create-classes' },
-            { text: 'createColumn', link: '/tables/helpers/create-column' },
-            { text: 'createConfig', link: '/tables/helpers/create-config' }
-          ]
-        },
-        {
-          text: 'Examples',
-          collapsible: true,
-          items: [
-            { text: 'Filtering Data', link: '/tables/examples/filtering-data' },
-            {
-              text: 'Linking Dropdown to Table',
-              link: '/tables/examples/linking-dropdown-to-table'
-            },
-            { text: 'Replacing Table Data', link: '/tables/examples/replacing-table-data' },
-            { text: 'Using Table DOM Nodes', link: '/tables/examples/using-table-dom-nodes' }
-          ]
-        }
-      ],
-      '/use/': [
-        {
-          text: '@screaming/use',
-          items: [{ text: 'Getting Started', link: '/use/' }]
-        },
-        {
-          text: 'Composables',
-          collapsible: true,
-          items: [
-            { text: 'useStepper', link: '/use/use-stepper' },
-            { text: 'useToggle', link: '/use/use-toggle' }
-          ]
-        }
-      ],
-      '/utils/': [
-        {
-          text: '@screaming/utils',
-          items: [{ text: 'Getting Started', link: '/utils/' }]
-        },
-        {
-          text: 'Array',
-          collapsible: true,
-          items: [
-            { text: 'groupsOf', link: '/utils/array/groups-of' },
-            { text: 'max', link: '/utils/array/max' },
-            { text: 'min', link: '/utils/array/min' },
-            { text: 'partition', link: '/utils/array/partition' },
-            { text: 'splitAt', link: '/utils/array/split-at' },
-            { text: 'sum', link: '/utils/array/sum' },
-            { text: 'toArray', link: '/utils/array/to-array' },
-            { text: 'uniq', link: '/utils/array/uniq' }
-          ]
-        },
-        {
-          text: 'Boolean',
-          collapsible: true,
-          items: [{ text: 'toBool', link: '/utils/boolean/to-bool' }]
-        },
-        {
-          text: 'DOM',
-          collapsible: true,
-          items: [
-            { text: 'createElement', link: '/utils/dom/create-element' },
-            { text: 'generateShareLinks', link: '/utils/dom/generate-share-links' },
-            { text: 'makeBackToTop', link: '/utils/dom/make-back-to-top' },
-            { text: 'scroll', link: '/utils/dom/scroll' }
-          ]
-        },
-        {
-          text: 'Function',
-          collapsible: true,
-          items: [
-            { text: 'debounce', link: '/utils/function/debounce' },
-            { text: 'memoize', link: '/utils/function/memoize' },
-            { text: 'once', link: '/utils/function/once' },
-            { text: 'throttle', link: '/utils/function/throttle' }
-          ]
-        },
-        {
-          text: 'Is',
-          collapsible: true,
-          items: [
-            { text: 'isArray', link: '/utils/is/is-array' },
-            { text: 'isBoolean', link: '/utils/is/is-boolean' },
-            { text: 'isDefined', link: '/utils/is/is-defined' },
-            { text: 'isEmptyObject', link: '/utils/is/is-empty-object' },
-            { text: 'isFunction', link: '/utils/is/is-function' },
-            { text: 'isNumber', link: '/utils/is/is-number' },
-            { text: 'isObject', link: '/utils/is/is-object' },
-            { text: 'isString', link: '/utils/is/is-string' },
-            { text: 'isTouchDevice', link: '/utils/is/is-touch-device' },
-            { text: 'isUnsupportedBrowser', link: '/utils/is/is-unsupported-browser' },
-            { text: 'isWindow', link: '/utils/is/is-window' }
-          ]
-        },
-        {
-          text: 'Maths',
-          collapsible: true,
-          items: [
-            { text: 'isEven', link: '/utils/maths/is-even' },
-            { text: 'isOdd', link: '/utils/maths/is-odd' },
-            { text: 'randomChance', link: '/utils/maths/random-chance' },
-            { text: 'randomNumber', link: '/utils/maths/random-number' },
-            { text: 'uniqueNumber', link: '/utils/maths/unique-number' }
-          ]
-        },
-        {
-          text: 'Miscellanous',
-          collapsible: true,
-          items: [
-            { text: 'easings', link: '/utils/misc/easings' },
-            { text: 'noop', link: '/utils/misc/noop' },
-            { text: 'sleep', link: '/utils/misc/sleep' }
-          ]
-        },
-        {
-          text: 'Object',
-          collapsible: true,
-          items: [
-            { text: 'deepMerge', link: '/utils/object/deep-merge' },
-            { text: 'rank', link: '/utils/object/rank' },
-            { text: 'sortKeys', link: '/utils/object/sort-keys' },
-            { text: 'sort', link: '/utils/object/sort' }
-          ]
-        },
-        {
-          text: 'String',
-          collapsible: true,
-          items: [
-            { text: 'capitalise', link: '/utils/string/capitalise' },
-            { text: 'chars', link: '/utils/string/chars' },
-            { text: 'isBlank', link: '/utils/string/is-blank' },
-            { text: 'isEmpty', link: '/utils/string/is-empty' },
-            { text: 'toCamel', link: '/utils/string/to-camel' },
-            { text: 'toKebab', link: '/utils/string/to-kebab' },
-            { text: 'toNumber', link: '/utils/string/to-number' },
-            { text: 'toOrdinal', link: '/utils/string/to-ordinal' },
-            { text: 'truncate', link: '/utils/string/truncate' }
-          ]
-        },
-        {
-          text: 'Types',
-          collapsible: true,
-          items: [{ text: 'types', link: '/utils/types/types' }]
-        }
-      ]
-    }
+    nav: createNav(),
+    sidebar: createSidebar()
   }
 })
+
+function createNav() {
+  return [
+    { text: 'Froggo', link: '/froggo/', activeMatch: '/froggo/*' },
+    { text: 'Share', link: '/share/', activeMatch: '/share/*' },
+    { text: 'Tables', link: '/tables/', activeMatch: '/tables/*' },
+    { text: 'Use', link: '/use/', activeMatch: '/use/*' },
+    { text: 'Utils', link: '/utils/', activeMatch: '/utils/*' }
+  ]
+}
+
+function createSidebar() {
+  return {
+    '/froggo/': createFroggoSidebar(),
+    '/share/': createShareSidebar(),
+    '/tables/': createTablesSidebar(),
+    '/use/': createUseSidebar(),
+    '/utils/': createUtilsSidebar()
+  }
+}
+
+function createFroggoSidebar() {
+  return [
+    {
+      text: '@screaming/froggo',
+      items: [{ text: 'Getting Started', link: '/froggo/' }]
+    },
+    {
+      text: 'Templates',
+      collapsible: true,
+      items: [
+        { text: 'Tailwind', link: '/froggo/tailwind' },
+        { text: 'SASS', link: '/froggo/sass' },
+        { text: 'NPM Package', link: '/froggo/package' }
+      ]
+    }
+  ]
+}
+
+function createShareSidebar() {
+  return [
+    {
+      text: '@screaming/share',
+      items: [{ text: 'Getting Started', link: '/share/' }]
+    },
+    {
+      text: 'Component',
+      collapsible: true,
+      items: [{ text: '<ShareLink />', link: '/share/share-link.md' }]
+    }
+  ]
+}
+
+function createTablesSidebar() {
+  return [
+    {
+      text: '@screaming/tables',
+      items: [{ text: 'Getting Started', link: '/tables/' }]
+    },
+    {
+      text: 'Components',
+      collapsible: true,
+      items: [
+        { text: '<Table />', link: '/tables/table' },
+        { text: '<MiniTable />', link: '/tables/mini-table' }
+      ]
+    },
+    {
+      text: 'Helpers',
+      collapsible: true,
+      items: [
+        { text: 'createClasses', link: '/tables/helpers/create-classes' },
+        { text: 'createColumn', link: '/tables/helpers/create-column' },
+        { text: 'createConfig', link: '/tables/helpers/create-config' },
+        { text: 'Data Attributes', link: '/tables/helpers/data-attributes' }
+      ]
+    },
+    {
+      text: 'Examples',
+      collapsible: true,
+      items: [
+        { text: 'Filtering Data', link: '/tables/examples/filtering-data' },
+        {
+          text: 'Linking Dropdown to Table',
+          link: '/tables/examples/linking-dropdown-to-table'
+        },
+        { text: 'Replacing Table Data', link: '/tables/examples/replacing-table-data' },
+        { text: 'Using Table DOM Nodes', link: '/tables/examples/using-table-dom-nodes' }
+      ]
+    }
+  ]
+}
+
+function createUseSidebar() {
+  return [
+    {
+      text: '@screaming/use',
+      items: [{ text: 'Getting Started', link: '/use/' }]
+    },
+    {
+      text: 'Composables',
+      collapsible: true,
+      items: [
+        { text: 'useStepper', link: '/use/use-stepper' },
+        { text: 'useToggle', link: '/use/use-toggle' }
+      ]
+    }
+  ]
+}
+
+function createUtilsSidebar() {
+  return [
+    {
+      text: '@screaming/utils',
+      items: [{ text: 'Getting Started', link: '/utils/' }]
+    },
+    {
+      text: 'Array',
+      collapsible: true,
+      items: [
+        { text: 'groupsOf', link: '/utils/array/groups-of' },
+        { text: 'max', link: '/utils/array/max' },
+        { text: 'min', link: '/utils/array/min' },
+        { text: 'partition', link: '/utils/array/partition' },
+        { text: 'splitAt', link: '/utils/array/split-at' },
+        { text: 'sum', link: '/utils/array/sum' },
+        { text: 'toArray', link: '/utils/array/to-array' },
+        { text: 'uniq', link: '/utils/array/uniq' }
+      ]
+    },
+    {
+      text: 'Boolean',
+      collapsible: true,
+      items: [{ text: 'toBool', link: '/utils/boolean/to-bool' }]
+    },
+    {
+      text: 'DOM',
+      collapsible: true,
+      items: [
+        { text: 'createElement', link: '/utils/dom/create-element' },
+        { text: 'generateShareLinks', link: '/utils/dom/generate-share-links' },
+        { text: 'makeBackToTop', link: '/utils/dom/make-back-to-top' },
+        { text: 'scroll', link: '/utils/dom/scroll' }
+      ]
+    },
+    {
+      text: 'Function',
+      collapsible: true,
+      items: [
+        { text: 'debounce', link: '/utils/function/debounce' },
+        { text: 'memoize', link: '/utils/function/memoize' },
+        { text: 'once', link: '/utils/function/once' },
+        { text: 'throttle', link: '/utils/function/throttle' }
+      ]
+    },
+    {
+      text: 'Is',
+      collapsible: true,
+      items: [
+        { text: 'isArray', link: '/utils/is/is-array' },
+        { text: 'isBoolean', link: '/utils/is/is-boolean' },
+        { text: 'isDefined', link: '/utils/is/is-defined' },
+        { text: 'isEmptyObject', link: '/utils/is/is-empty-object' },
+        { text: 'isFunction', link: '/utils/is/is-function' },
+        { text: 'isNumber', link: '/utils/is/is-number' },
+        { text: 'isObject', link: '/utils/is/is-object' },
+        { text: 'isString', link: '/utils/is/is-string' },
+        { text: 'isTouchDevice', link: '/utils/is/is-touch-device' },
+        { text: 'isUnsupportedBrowser', link: '/utils/is/is-unsupported-browser' },
+        { text: 'isWindow', link: '/utils/is/is-window' }
+      ]
+    },
+    {
+      text: 'Maths',
+      collapsible: true,
+      items: [
+        { text: 'isEven', link: '/utils/maths/is-even' },
+        { text: 'isOdd', link: '/utils/maths/is-odd' },
+        { text: 'randomChance', link: '/utils/maths/random-chance' },
+        { text: 'randomNumber', link: '/utils/maths/random-number' },
+        { text: 'uniqueNumber', link: '/utils/maths/unique-number' }
+      ]
+    },
+    {
+      text: 'Miscellanous',
+      collapsible: true,
+      items: [
+        { text: 'easings', link: '/utils/misc/easings' },
+        { text: 'noop', link: '/utils/misc/noop' },
+        { text: 'sleep', link: '/utils/misc/sleep' }
+      ]
+    },
+    {
+      text: 'Object',
+      collapsible: true,
+      items: [
+        { text: 'deepMerge', link: '/utils/object/deep-merge' },
+        { text: 'rank', link: '/utils/object/rank' },
+        { text: 'sortKeys', link: '/utils/object/sort-keys' },
+        { text: 'sort', link: '/utils/object/sort' }
+      ]
+    },
+    {
+      text: 'String',
+      collapsible: true,
+      items: [
+        { text: 'capitalise', link: '/utils/string/capitalise' },
+        { text: 'chars', link: '/utils/string/chars' },
+        { text: 'isBlank', link: '/utils/string/is-blank' },
+        { text: 'isEmpty', link: '/utils/string/is-empty' },
+        { text: 'toCamel', link: '/utils/string/to-camel' },
+        { text: 'toKebab', link: '/utils/string/to-kebab' },
+        { text: 'toNumber', link: '/utils/string/to-number' },
+        { text: 'toOrdinal', link: '/utils/string/to-ordinal' },
+        { text: 'truncate', link: '/utils/string/truncate' }
+      ]
+    },
+    {
+      text: 'Types',
+      collapsible: true,
+      items: [{ text: 'types', link: '/utils/types/types' }]
+    }
+  ]
+}
